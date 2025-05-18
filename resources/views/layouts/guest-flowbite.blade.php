3f1c1705084e561eec4c85b4539eafec56c38067
rizkyadiryanto14 <adiryantorizky140820@gmail.com>
2025-05-18 20:24:45 +0700

    update : update title guest-flowbite.blade.php

--- resources/views/layouts/guest-flowbite.blade.php
+++ resources/views/layouts/guest-flowbite.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'E-Presensi') }}</title>
+    <title>E-Presensi - SDN BAJO</title>
 
     <link rel="shortcut icon" href="{{ asset('assets/images/logo_presensi.png') }}" type="image/x-icon" />
 
